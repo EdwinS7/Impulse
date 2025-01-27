e438c8039cfb86c543ae8284c9ed38ebd9b1ccec
Edwn <88218388+EdwinS7@users.noreply.github.com>
2025-01-26 20:50:04 -0500

    Update Wrapper.hpp

--- Source/Core/LuaU/Wrapper.hpp
+++ Source/Core/LuaU/Wrapper.hpp
@@ -33,78 +33,14 @@ namespace Wrapper {
 
     static std::unordered_map<int, int> OriginalFunctions;
 
+    // NOTE: Temp removed cuz it no work
     // NOTE: Pasted from Synapse X / https://github.com/Acrillis/SynapseX/blob/master/Synapse/Src/Exploit/Execution/RbxApi.cpp#L1591
     int HookFunction(lua_State* L /*, const char* file_path, const char* content*/) {
-        luaL_checktype(L, 1, LUA_TFUNCTION);
-        luaL_checktype(L, 2, LUA_TFUNCTION);
-
-        if (lua_iscfunction(L, 1)) {
-            if (!lua_iscfunction(L, 2)) {
-                luaL_argerror(L, 2, "C function expected");
-                return 0;
-            }
-
-            // Backup original C function
-            lua_pushvalue(L, 1);
-            int OriginalRef = lua_ref(L, LUA_REGISTRYINDEX);
-
-            // Hook the function by replacing its implementation
-            lua_pushvalue(L, 2);
-            lua_rawseti(L, LUA_REGISTRYINDEX, OriginalRef);
-
-            // Return the original reference for potential restoration
-            lua_pushinteger(L, OriginalRef);
-
-            return 1;
-        }
-
-        if (lua_iscfunction(L, 2)) {
-            luaL_argerror(L, 2, "Lua function expected");
-            return 0;
-        }
-
-        lua_pushvalue(L, 1);
-        int OriginalRef = lua_ref(L, LUA_REGISTRYINDEX);
-
-        int HookUpvalues = lua_getupvalue(L, 2, 1) ? lua_gettop(L) : 0;
-        int OriginalUpvalues = lua_getupvalue(L, 1, 1) ? lua_gettop(L) : 0;
-
-        if (HookUpvalues > OriginalUpvalues) {
-            luaL_argerror(L, 2, "hook function has too many upvalues");
-            return 0;
-        }
-
-        // Replace the original function with the hook
-        lua_pushvalue(L, 2);
-        lua_rawseti(L, LUA_REGISTRYINDEX, OriginalRef);
-
-        // Update upvalues
-        for (int i = 1; i <= HookUpvalues; i++) {
-            const char* name = lua_getupvalue(L, 2, i);
-            if (name) lua_setupvalue(L, 1, i);
-        }
-
-        // Return the original reference for potential restoration
-        lua_pushinteger(L, OriginalRef);
-
-        return 1;
+        
     }
 
     int RestoreFunction(lua_State* L) {
-        intptr_t funcRef = reinterpret_cast<intptr_t>(lua_touserdata(L, 1));
-
-        auto it = OriginalFunctions.find(funcRef);
-
-        if (it != OriginalFunctions.end()) {
-            OriginalFunctions.erase(it);
-            lua_pushboolean(L, true);
-
-            return 1;
-        }
-
-        lua_pushboolean(L, false);
-
-        return 1;
+        
     }
 }
 
